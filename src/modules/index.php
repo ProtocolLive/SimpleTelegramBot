4bc98e00ae0f174b9e9e876996813e23801537ae
Protocol Corporation <protocollive@outlook.com>
2022-04-06 12:02:33 -0300

    Interface for uninstall

--- src/modules/index.php
+++ src/modules/index.php
@@ -1,10 +1,17 @@
 <?php
 //Protocol Corporation Ltda.
 //https://github.com/ProtocolLive/SimpleTelegramBot
-//2022.03.16.00
+//2022.03.21.00
 
 interface InterfaceModule{
   static public function Install(
+    TelegramBotLibrary $Bot,
+    TgCallback $Webhook,
+    StbSysDatabase $Db,
+    StbLanguage $Lang
+  );
+
+  static public function Uninstall(
     TelegramBotLibrary $Bot,
     TgCallback $Webhook,
     StbSysDatabase $Db,
