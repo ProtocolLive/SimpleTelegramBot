d83f97e794175fd143c1f0ab9f1e32708687539d
Protocol Corporation <protocollive@outlook.com>
2022-04-26 12:56:23 -0300

    Created a simple install page

--- src/config.php
+++ src/config.php
@@ -1,46 +1,37 @@
 <?php
 //Protocol Corporation Ltda.
 //https://github.com/ProtocolLive/SimpleTelegramBot
-//2022.04.22.00
+//2022.04.26.00
 
-//PHP
-/**
- * Type: string
- * Default: UTC
- */
-const Timezone = 'UTC';
+//Installation date: ##DATE##
 
-/**
- * Token given by @BotFather
- * Type: string
- */
-const Token = '';
+//Type: string
+//Default: UTC
+const Timezone = '##TIMEZONE##';
+
+//Token given by @BotFather
+//Type: string
+const Token = '##TOKEN##';
 
 //Use the test server
 //Type: bool
 //Default: false
-const TestServer = false;
+const TestServer = ##TESTSERVER##;
 
-/**
- * Default language
- * Type: string
- * Default: en
- */
-const DefaultLanguage = 'en';
+//Default language
+//Type: string
+//Default: en
+const DefaultLanguage = '##LANGUAGE##';
 
-/**
- * Main admin
- * Type: int
- */
-const Admin = 0;
+//Main admin
+//Type: int
+const Admin = ##ADMIN##;
 
-/**
- * Use the debug constants of the class TelegramBot or/and the constants defined to bot
- * Tips: & and, | or, ^ xor
- * Type: TblDebug
- * Default: TblDebug::All
- */
-const Debug = TblDebug::All;
+//Use the debug constants of the class TelegramBot or/and the constants defined to bot
+//Tips: & and, | or, ^ xor
+//Type: TblDebug
+//Default: TblDebug::All
+const Debug = TblDebug::All ^ TblDebug::Curl;
 
 //Default currency
 //Type: TgInvoiceCurrencies 
---
+++ src/install.php
@@ -0,0 +1,74 @@
+<?php
+//Protocol Corporation Ltda.
+//https://github.com/ProtocolLive/SimpleTelegramBot
+//2022.04.26.00
+
+$_GET['a'] ??= '';
+if(basename(__FILE__) !== 'install.php'):
+  echo 'Protocol SimpleTelegramBot already installed!';
+elseif(function_exists('Action_' . $_GET['a'])):
+  call_user_func('Action_' . $_GET['a']);
+endif;
+
+function Action_():void{?>
+  <form method="post" action="install.php?a=ok">
+    <table>
+      <tr>
+        <td>Timezone:</td>
+        <td>
+          <select name="timezone"><?php
+            foreach(DateTimeZone::listIdentifiers(DateTimeZone::ALL) as $name):?>
+              <option value="<?php echo $name;?>"><?php echo $name;?></option><?php
+            endforeach;?>
+          </select>
+        </td>
+      </tr>
+      <tr>
+        <td>Token:</td>
+        <td>
+          <input type="text" name="token">
+        </td>
+      </tr>
+      <tr>
+        <td>Test server:</td>
+        <td>
+          <select name="testserver">
+            <option value="false">No</option>
+            <option value="true">Yes</option>
+          </select>
+        </td>
+      </tr>
+      <tr>
+        <td>Default language:</td>
+        <td>
+          <select name="language">
+            <option value="en">English</option>
+            <option value="pt-br">Portuguese Brazil</option>
+          </select>
+        </td>
+      </tr>
+      <tr>
+        <td>Admin ID:</td>
+        <td>
+          <input type="text" name="admin">
+        </td>
+      </tr>
+    </table>
+    <p>
+      <input type="submit" value="Install">
+    </p>
+  </form><?php
+}
+
+function Action_ok():void{
+  $config = file_get_contents(__DIR__ . '/config.php');
+  $config = str_replace('##DATE##', date('Y-m-d H:i:s'), $config);
+  $config = str_replace('##TIMEZONE##', $_POST['timezone'], $config);
+  $config = str_replace('##TOKEN##', $_POST['token'], $config);
+  $config = str_replace('##TESTSERVER##', $_POST['testserver'], $config);
+  $config = str_replace('##LANGUAGE##', $_POST['language'], $config);
+  $config = str_replace('##ADMIN##', $_POST['admin'], $config);
+  file_put_contents(__DIR__ . '/config.php', $config);
+  rename(__DIR__ . '/install.php', __DIR__ . '/_install.php');
+  echo 'Install complete!';
+}
