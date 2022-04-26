d3e6ae9b0254197ff802b847ffa0aec4e9d81aed
Protocol Corporation <protocollive@outlook.com>
2022-04-26 16:19:44 -0300

    File moved and using call_user_func

--- src/RENAME_WITH_TOKEN/cron.php
+++ src/RENAME_WITH_TOKEN/cron.php
@@ -1,11 +1,11 @@
 <?php
 //Protocol Corporation Ltda.
 //https://github.com/ProtocolLive/SimpleTelegramBot
-//2022.03.21.00
+//2022.04.26.00
 
 require(__DIR__ . '/system/system.php');
 
 ArgV();
 $start = microtime(true);
-$_SERVER['Cron']();
+call_user_func($_SERVER['Cron']);
 LogBot('Cron time: ' . (microtime(true) - $start));
