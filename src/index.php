9007dd8a46cdd198568a27a2f978dcad3b83dcf3
Protocol Corporation <protocollive@outlook.com>
2022-04-26 21:19:34 -0300

    Install renamed to index

--- src/index.php
+++ src/index.php
@@ -1,21 +1,21 @@
 <?php
 //Protocol Corporation Ltda.
 //https://github.com/ProtocolLive/SimpleTelegramBot
-//2022.04.26.03
+//2022.04.26.04
 
 require(__DIR__ . '/system/php.php');
 set_error_handler('error');
 set_exception_handler('error');
 
 $_GET['a'] ??= '';
-if(basename(__FILE__) !== 'install.php'):
+if(basename(__FILE__) !== 'index.php'):
   echo 'Protocol SimpleTelegramBot already installed!';
 elseif(function_exists('Action_' . $_GET['a'])):
   call_user_func('Action_' . $_GET['a']);
 endif;
 
 function Action_():void{?>
-  <form method="post" action="install.php?a=ok">
+  <form method="post" action="index.php?a=ok">
     <table>
       <tr>
         <td>Timezone:</td>
@@ -75,7 +75,7 @@ function Action_ok():void{
   file_put_contents(__DIR__ . '/config.php', $config);
   $token = explode(':', $_POST['token']);
   rename(__DIR__ . '/RENAME_WITH_TOKEN', __DIR__ . '/' . $token[1]);
-  rename(__DIR__ . '/install.php', __DIR__ . '/_install.php');
+  rename(__DIR__ . '/index.php', __DIR__ . '/install.php');
   echo '✅ Install complete!';
   $url = dirname($_SERVER['SCRIPT_URI']);
   $url .= '/' . $token[1] . '/index.php?a=WebhookSet';
