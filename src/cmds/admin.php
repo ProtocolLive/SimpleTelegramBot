20f9914db48b3bb80680332bb3814a32cc1e2dc5
Protocol Corporation <protocollive@outlook.com>
2022-04-29 10:06:06 -0300

    Improving the admin menu

--- src/cmds/admin.php
+++ src/cmds/admin.php
@@ -1,15 +1,16 @@
 <?php
 //Protocol Corporation Ltda.
 //https://github.com/ProtocolLive/SimpleTelegramBot
-//2022.04.29.00
+//2022.04.29.01
 
 function Command_id():void{
   /**
    * @var TelegramBotLibrary $Bot
-   * @var StbLanguage $Lang
+   * @var StbLanguageSys $Lang
    * @var TblCmd $Webhook
    */
   global $Bot, $Lang, $Webhook;
+  DebugTrace();
   $Bot->TextSend(
     $Webhook->Message->User->Id,
     sprintf(
@@ -20,16 +21,20 @@ function Command_id():void{
 }
 
 function Command_admin():void{
+  Callback_admin();
+}
+
+function Callback_admin():void{
   /**
    * @var TelegramBotLibrary $Bot
-   * @var TblCmd $Webhook
-   * @var StbDatabaseSys $Db
-   * @var StbLanguage $Lang
+   * @var TblCmd|TgCallback $Webhook
+   * @var StbLanguageSys $Lang
    */
-  global $Bot, $Webhook, $Db, $Lang;
-  if($Webhook->Message->User->Id !== Admin):
+  global $Bot, $Webhook, $Lang;
+  DebugTrace();
+  if(($Webhook->User->Id ?? $Webhook->Message->User->Id) !== Admin):
     $Bot->TextSend(
-      $Webhook->Message->User->Id,
+      $Webhook->User->Id ?? $Webhook->Message->User->Id,
       $Lang->Get('Denied')
     );
     return;
@@ -37,6 +42,48 @@ function Command_admin():void{
   $mk = new TblMarkupInline();
   $line = 0;
   $col = 0;
+  $mk->ButtonCallback(
+    $line,
+    $col++,
+    $Lang->Get('AdminsButton', Group: 'Admin'),
+    'Admins'
+  );
+  if(get_class($Webhook) === 'TblCmd'):
+    $Bot->TextSend(
+      Admin,
+      $Lang->Get('AdminMenu', Group: 'Admin'),
+      Markup: $mk
+    );
+  else:
+    $Bot->TextEdit(
+      Admin,
+      $Webhook->Message->Id,
+      $Lang->Get('AdminMenu', Group: 'Admin'),
+      Markup: $mk
+    );
+  endif;
+}
+
+function Callback_admins():void{
+  /**
+   * @var TelegramBotLibrary $Bot
+   * @var TgCallback $Webhook
+   * @var StbDatabaseSys $Db
+   * @var StbLanguageSys $Lang
+   */
+  global $Bot, $Webhook, $Db, $Lang;
+  DebugTrace();
+  if($Webhook->User->Id !== Admin):
+    $Bot->TextSend(
+      $Webhook->User->Id,
+      $Lang->Get('Denied')
+    );
+    return;
+  endif;
+  $mk = new TblMarkupInline();
+  $line = 0;
+  $col = 0;
+  $mk->ButtonCallback($line, $col++, '🔙', 'admin');
   $mk->ButtonCallback($line, $col++, '➕', 'AdminNew');
 
   $Admins = [Admin => 0] + $Db->Admins();
@@ -49,8 +96,9 @@ function Command_admin():void{
     endif;
     $mk->ButtonCallback($line, $col++, $detail, 'Admin ' . $admin);
   endforeach;
-  $Bot->TextSend(
+  $Bot->TextEdit(
     Admin,
+    $Webhook->Message->Id,
     $Lang->Get('Admins', Group: 'Admin'),
     Markup: $mk
   );
