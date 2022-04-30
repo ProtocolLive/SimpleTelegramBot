95cab4f4403e632412eb4e9525b25f440873aacc
Protocol Corporation <protocollive@outlook.com>
2022-04-30 06:56:16 -0300

    Interface remved

--- src/RENAME_WITH_TOKEN/modules/index.php
+++ src/RENAME_WITH_TOKEN/modules/index.php
@@ -1,21 +1,7 @@
 <?php
 //Protocol Corporation Ltda.
 //https://github.com/ProtocolLive/SimpleTelegramBot
-//2022.04.29.00
-
-interface StbModuleInterface{
-  static public function Install(
-    TelegramBotLibrary $Bot,
-    TgCallback $Webhook,
-    StbDatabaseSys $Db
-  );
-
-  static public function Uninstall(
-    TelegramBotLibrary $Bot,
-    TgCallback $Webhook,
-    StbDatabaseSys $Db
-  );
-}
+//2022.04.30.00
 
 abstract class StbModuleTools{
   static public function CommandDel(
