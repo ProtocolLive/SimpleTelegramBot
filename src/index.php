0cd434323bf81b6067c9ec80dea679155b78abaf
Protocol Corporation <protocollive@outlook.com>
2022-04-30 07:01:01 -0300

    Styling, config moved, DirToken removed, fix db creation and using __FILE__

--- src/index.php
+++ src/index.php
@@ -1,7 +1,7 @@
 <?php
 //Protocol Corporation Ltda.
 //https://github.com/ProtocolLive/SimpleTelegramBot
-//2022.04.29.00
+//2022.04.30.00
 
 require(__DIR__ . '/system/php.php');
 set_error_handler('error');
@@ -47,7 +47,7 @@ function Action_():void{?>
             <option value="en">English</option>
             <option value="pt-br">Portuguese Brazil</option>
           </select><br>
-          Another language? Add later in config.php
+          <span style="font-size:12">Another language? Add later in config_bot.php</span>
         </td>
       </tr>
       <tr>
@@ -71,19 +71,18 @@ function Action_ok():void{
   $token = explode(':', $_POST['token']);
   $token = $token[1];
 
-  $config = file_get_contents(__DIR__ . '/config.php');
+  $config = file_get_contents(__DIR__ . '/RENAME_WITH_TOKEN/config.php');
   $config = str_replace('##DATE##', date('Y-m-d H:i:s'), $config);
   $config = str_replace('##TIMEZONE##', $_POST['timezone'], $config);
   $config = str_replace('##TOKEN##', $_POST['token'], $config);
   $config = str_replace('##TESTSERVER##', $_POST['testserver'], $config);
   $config = str_replace('##LANGUAGE##', $_POST['language'], $config);
   $config = str_replace('##ADMIN##', $_POST['admin'], $config);
-  $config = str_replace('##DIRTOKEN##', __DIR__ . '/' . $token, $config);
-  file_put_contents(__DIR__ . '/config.php', $config);
+  file_put_contents(__DIR__ . '/RENAME_WITH_TOKEN/config.php', $config);
 
+  file_put_contents(__DIR__ . '/RENAME_WITH_TOKEN/db/system.php', '{}');
   rename(__DIR__ . '/RENAME_WITH_TOKEN', __DIR__ . '/' . $token);
-  file_put_contents(__DIR__ . '/' . $token . '/system.php', '{}');
-  rename(__DIR__ . '/index.php', __DIR__ . '/install.php');
+  rename(__FILE__, __DIR__ . '/install.php');
 
   echo '✅ Install complete!';
   $url = dirname($_SERVER['SCRIPT_URI']);
