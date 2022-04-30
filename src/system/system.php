b3c0d359ab7c48df510847a3c0bebbff9e0cd395
Protocol Corporation <protocollive@outlook.com>
2022-04-30 08:13:47 -0300

    DirLogs and DirModules

--- src/system/system.php
+++ src/system/system.php
@@ -1,7 +1,7 @@
 <?php
 //Protocol Corporation Ltda.
 //https://github.com/ProtocolLive/SimpleTelegramBot
-//2022.04.30.00
+//2022.04.30.01
 
 require(__DIR__ . '/php.php');
 require(__DIR__ . '/requires.php');
@@ -10,23 +10,21 @@ set_error_handler('HandlerError');
 set_exception_handler('HandlerException');
 date_default_timezone_set(Timezone);
 
-ini_set('error_log', DirToken . '/logs/error.log');
-$DebugTraceFolder = DirToken . '/logs';
+ini_set('error_log', DirLogs . '/error.log');
+$DebugTraceFolder = DirLogs;
 
 $BotData = new TblData(
   Token,
-  DirToken . '/logs',
+  DirLogs,
   Debug,
   TestServer
 );
 $Bot = new TelegramBotLibrary($BotData, TestServer);
-$Db = new StbDatabaseSys(DirToken);
+$Db = new StbDatabaseSys;
 $Lang = new StbLanguageSys(DefaultLanguage);
 $UserLang = DefaultLanguage;
 
 //Load modules
 foreach($Db->Modules() as $module => $install):
-  require(DirToken . '/modules/' . $module . '/index.php');
+  require(DirModules . '/' . $module . '/index.php');
 endforeach;
-
-const DirModules = DirToken . '/modules';
