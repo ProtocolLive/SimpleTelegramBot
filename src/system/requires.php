a6a44fa7e43e7574f459ca8b35e1cf558b23cb9d
Protocol Corporation <protocollive@outlook.com>
2022-04-30 12:20:51 -0300

    File moved

--- src/system/requires.php
+++ src/system/requires.php
@@ -1,7 +1,7 @@
 <?php
 //Protocol Corporation Ltda.
 //https://github.com/ProtocolLive/SimpleTelegramBot
-//2022.04.30.00
+//2022.04.30.01
 
 require(__DIR__ . '/constants.php');
 require(DirSystem . '/system/functions/debug.php');
@@ -9,9 +9,8 @@ require(DirSystem . '/system/functions/basics.php');
 require(DirSystem . '/system/functions/bot.php');
 require(DirSystem . '/system/functions/database.php');
 require(DirSystem . '/system/functions/language.php');
+require(DirSystem . '/system/functions/modules.php');
 require(DirSystem . '/class/TelegramBotLibrary.php');
-
-require(DirToken . '/modules/index.php');
 
 require(DirSystem . '/cmds/admin.php');
 require(DirSystem . '/cmds/modules.php');
