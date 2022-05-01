4e438405ece2f695c8089e795e740fa3d5a5aa07
Protocol Corporation <protocollive@outlook.com>
2022-05-01 09:44:29 -0300

    Adaptations for cron in DirToken index

--- src/RENAME_WITH_TOKEN/index.php
+++ src/RENAME_WITH_TOKEN/index.php
@@ -1,9 +1,14 @@
 <?php
 //Protocol Corporation Ltda.
 //https://github.com/ProtocolLive/SimpleTelegramBot
-//2022.04.30.01
+//2022.05.01.00
 
 define('DirToken', __DIR__);
 require(dirname(__DIR__, 1) . '/system/system.php');
 
-require(DirSystem . '/entry.php');
+ArgV();
+if(isset($_SERVER['Cron'])):
+  require(DirSystem . '/cron.php');
+else:
+  require(DirSystem . '/entry.php');
+endif;
--- src/cron.php
+++ src/cron.php
@@ -1,13 +1,8 @@
 <?php
 //Protocol Corporation Ltda.
 //https://github.com/ProtocolLive/SimpleTelegramBot
-//2022.04.27.00
+//2022.05.01.00
 
-require(__DIR__ . '/system/system.php');
-
-ArgV();
-if(isset($_SERVER['Cron'])):
-  $start = microtime(true);
-  call_user_func($_SERVER['Cron']);
-  LogBot('Cron time: ' . (microtime(true) - $start));
-endif;
+$start = microtime(true);
+call_user_func($_SERVER['Cron']);
+LogBot('Cron time: ' . (microtime(true) - $start));
