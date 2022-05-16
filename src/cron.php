ecf6ca23c71602ef5f9eb5cf99aa7d432aa426b5
Protocol Corporation <protocollive@outlook.com>
2022-05-16 09:14:18 -0300

    Loading module

--- src/cron.php
+++ src/cron.php
@@ -1,8 +1,9 @@
 <?php
 //Protocol Corporation Ltda.
 //https://github.com/ProtocolLive/SimpleTelegramBot
-//2022.05.01.00
+//2022.05.16.00
 
 $start = microtime(true);
+StbModuleLoad($_SERVER['Cron']);
 call_user_func($_SERVER['Cron']);
 LogBot('Cron time: ' . (microtime(true) - $start));
