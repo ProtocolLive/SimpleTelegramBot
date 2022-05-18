ee4f2a9070ab9c49583d72a87b472ff035524f7a
Protocol Corporation <protocollive@outlook.com>
2022-05-18 08:37:10 -0300

    Fix cron call

--- src/cron.php
+++ src/cron.php
@@ -1,9 +1,9 @@
 <?php
 //Protocol Corporation Ltda.
 //https://github.com/ProtocolLive/SimpleTelegramBot
-//2022.05.16.00
+//2022.05.18.00
 
 $start = microtime(true);
 StbModuleLoad($_SERVER['Cron']);
-call_user_func($_SERVER['Cron']);
+call_user_func($_SERVER['Cron'] . '::Cron');
 LogBot('Cron time: ' . (microtime(true) - $start));
