53c2d082a60829899bcbd2c07515e5aa588e9fb1
Protocol Corporation <protocollive@outlook.com>
2022-05-17 23:43:06 -0300

    New function StbModuleSystem and load module by class name

--- src/functions/module.php
+++ src/functions/module.php
@@ -1,16 +1,14 @@
 <?php
 //Protocol Corporation Ltda.
 //https://github.com/ProtocolLive/SimpleTelegramBot
-//2022.05.15.00
+//2022.05.17.00
 
-function StbModuleLoad(string $Method):bool{
-  if(strpos($Method, '::') === false):
-    return false;
+function StbModuleSystem():array{
+  return ['Admin', 'Modules'];
+}
+
+function StbModuleLoad(string $Module):void{
+  if(in_array($Module, StbModuleSystem()) === false):
+    require(DirModules . '/' . $Module . '/index.php');
   endif;
-  $module = explode('::', $Method);
-  if(count($module) === 1):
-    return false;
-  endif;
-  require(DirModules . '/' . $module[0] . '/index.php');
-  return true;
 }
