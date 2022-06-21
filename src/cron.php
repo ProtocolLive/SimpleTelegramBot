944a0a973b4012db19cae4d8bfb187f90e57fdd2
Protocol Corporation <protocollive@outlook.com>
2022-06-21 17:01:52 -0300

    Using the time informed by server

--- src/cron.php
+++ src/cron.php
@@ -1,9 +1,8 @@
 <?php
 //Protocol Corporation Ltda.
 //https://github.com/ProtocolLive/SimpleTelegramBot
-//2022.05.18.00
+//2022.06.20.00
 
-$start = microtime(true);
 StbModuleLoad($_SERVER['Cron']);
 call_user_func($_SERVER['Cron'] . '::Cron');
-LogBot('Cron time: ' . (microtime(true) - $start));
+LogBot('Cron time: ' . (microtime(true) - $_SERVER['REQUEST_TIME_FLOAT']));
