ede63ce30246424b3e5644bf3d6a5a6ab0c03f61
Protocol Corporation <protocollive@outlook.com>
2022-07-30 10:18:15 -0300

    Cron logs

--- src/functions/bot.php
+++ src/functions/bot.php
@@ -1,10 +1,10 @@
 <?php
 //Protocol Corporation Ltda.
 //https://github.com/ProtocolLive/FuncoesComuns
-//2022.07.29.00
+//2022.07.30.00
 
 /**
- * Log an event in bot file
+ * Log an event in bot log file
  */
 function LogBot(string $Msg, bool $NewLine = true):void{
   DebugTrace();
@@ -13,6 +13,18 @@ function LogBot(string $Msg, bool $NewLine = true):void{
     $Msg .= PHP_EOL;
   endif;
   file_put_contents(DirLogs . '/bot.log', $Msg, FILE_APPEND);
+}
+
+/**
+ * Log the cron job
+ */
+function LogCron(string $Msg, bool $NewLine = true):void{
+  DebugTrace();
+  $Msg = date('Y-m-d H:i:s') . PHP_EOL . $Msg . PHP_EOL;
+  if($NewLine):
+    $Msg .= PHP_EOL;
+  endif;
+  file_put_contents(DirLogs . '/cron.log', $Msg, FILE_APPEND);
 }
 
 function SendUserCmd(string $Command, string $EventAdditional = null):bool{
