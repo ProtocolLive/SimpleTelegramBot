e96734fbeb5bdef0552187459be684ecf84668f6
Protocol Corporation <protocollive@outlook.com>
2022-07-30 12:06:46 -0300

    Using const instead define

--- src/RENAME_WITH_TOKEN/index.php
+++ src/RENAME_WITH_TOKEN/index.php
@@ -1,9 +1,9 @@
 <?php
 //Protocol Corporation Ltda.
 //https://github.com/ProtocolLive/SimpleTelegramBot
-//2022.05.01.00
+//2022.07.30.00
 
-define('DirToken', __DIR__);
+const DirToken = __DIR__;
 require(dirname(__DIR__, 1) . '/system/system.php');
 
 ArgV();
