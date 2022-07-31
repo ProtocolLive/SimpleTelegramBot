c209c079277f68566f3aee745762b8ba7059a78b
Protocol Corporation <protocollive@outlook.com>
2022-07-31 09:49:40 -0300

    Changed log type

--- src/cron.php
+++ src/cron.php
@@ -1,8 +1,8 @@
 <?php
 //Protocol Corporation Ltda.
 //https://github.com/ProtocolLive/SimpleTelegramBot
-//2022.06.20.00
+//2022.07.31.00
 
 StbModuleLoad($_SERVER['Cron']);
 call_user_func($_SERVER['Cron'] . '::Cron');
-LogBot('Cron time: ' . (microtime(true) - $_SERVER['REQUEST_TIME_FLOAT']));
+LogCron('Cron time: ' . (microtime(true) - $_SERVER['REQUEST_TIME_FLOAT']));
