a2af1e66fa3165a383ed15c1ac2233a8c267f98e
Protocol Corporation <protocollive@outlook.com>
2022-08-25 18:05:27 -0300

    Autoload

--- src/system/system.php
+++ src/system/system.php
@@ -1,12 +1,24 @@
 <?php
 //Protocol Corporation Ltda.
 //https://github.com/ProtocolLive/SimpleTelegramBot
-//2022.08.22.01
+//2022.08.25.00
 
 require(__DIR__ . '/php.php');
 require(__DIR__ . '/requires.php');
+
+if(is_file(DirSystem . '/vendor/autoload.php')):
+  require(DirSystem . '/vendor/autoload.php');
+else:
+  spl_autoload_register(function (string $class){
+    $class = str_replace('ProtocolLive\TelegramBotLibrary', DirSystem . '/vendor/protocollive/telegrambotlibrary/src', $class);
+    $class = str_replace('\\', '/', $class);
+    require($class . '.php');
+  });
+endif;
+
 require(DirToken . '/config.php');
 
+use ProtocolLive\TelegramBotLibrary\TelegramBotLibrary;
 use ProtocolLive\TelegramBotLibrary\TblObjects\TblData;
 use ProtocolLive\TelegramBotLibrary\StbObjects\{StbDatabase, StbLanguageSys};
 
