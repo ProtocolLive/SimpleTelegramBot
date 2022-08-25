071e3c1bf49c90a8389ddc28d78a207ca1ce1758
Protocol Corporation <protocollive@outlook.com>
2022-08-25 18:08:13 -0300

    Class in autoload

--- src/system/requires.php
+++ src/system/requires.php
@@ -1,13 +1,12 @@
 <?php
 //Protocol Corporation Ltda.
 //https://github.com/ProtocolLive/SimpleTelegramBot
-//2022.08.22.01
+//2022.08.23.00
 
 require(__DIR__ . '/constants.php');
 require(DirSystem . '/system/functions/debug.php');
 require(DirSystem . '/system/functions/basics.php');
 require(DirSystem . '/system/PhpLiveDb/index.php');
-require(DirSystem . '/system/TelegramBotLibrary/index.php');
 
 require(DirSystem . '/system/StbObjects/StbDatabase.php');
 require(DirSystem . '/system/StbObjects/StbDbAdminData.php');
@@ -18,7 +17,6 @@ require(DirSystem . '/system/StbObjects/StbLanguageModule.php');
 require(DirSystem . '/system/StbObjects/StbLanguageSys.php');
 
 require(DirSystem . '/functions/bot.php');
-require(DirSystem . '/functions/language.php');
 require(DirSystem . '/functions/module.php');
 
 require(DirSystem . '/cmds/admin.php');
