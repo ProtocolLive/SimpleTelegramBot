54415c1e3693db90971bac704b8f829f4a3c4921
Protocol Corporation <protocollive@outlook.com>
2022-08-28 18:55:31 -0300

    Added autoload for STB classes

--- src/system/system.php
+++ src/system/system.php
@@ -1,25 +1,33 @@
 <?php
 //Protocol Corporation Ltda.
 //https://github.com/ProtocolLive/SimpleTelegramBot
-//2022.08.28.00
-
-require(__DIR__ . '/php.php');
-require(__DIR__ . '/requires.php');
-
-spl_autoload_register(function (string $class){
-  $class = str_replace('ProtocolLive\TelegramBotLibrary', DirSystem . '/vendor/protocollive/telegrambotlibrary/src', $class);
-  $class = str_replace('\\', '/', $class);
-  require($class . '.php');
-});
-
-require(DirToken . '/config.php');
+//2022.08.28.01
 
 use ProtocolLive\SimpleTelegramBot\StbObjects\{StbDatabase, StbLanguageSys};
 use ProtocolLive\TelegramBotLibrary\TblObjects\TblData;
 use ProtocolLive\TelegramBotLibrary\TelegramBotLibrary;
 
+require(__DIR__ . '/php.php');
+require(__DIR__ . '/requires.php');
 set_error_handler('HandlerError');
 set_exception_handler('HandlerException');
+
+spl_autoload_register(function (string $Class){
+  $Class = str_replace(
+    'ProtocolLive\TelegramBotLibrary',
+    DirSystem . '/vendor/protocollive/telegrambotlibrary/src',
+    $Class
+  );
+  $Class = str_replace(
+    'ProtocolLive\SimpleTelegramBot\StbObjects',
+    DirSystem . '/system/StbObjects',
+    $Class
+  );
+  $Class = str_replace('\\', '/', $Class);
+  require($Class . '.php');
+});
+
+require(DirToken . '/config.php');
 date_default_timezone_set(Timezone);
 
 ini_set('error_log', DirLogs . '/error.log');
