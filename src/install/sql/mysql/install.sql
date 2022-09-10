6d2360274520eeb95e2fa825bbaebd44fb6d591d
Protocol Corporation <protocollive@outlook.com>
2022-09-10 13:29:59 -0300

    Added charset, fix int unsigned and module name

--- src/install/sql/mysql/install.sql
+++ src/install/sql/mysql/install.sql
@@ -1,7 +1,8 @@
 create table callbackshash(
   hash varchar(100) not null primary key,
   method varchar(100) not null
-);
+) engine=InnoDB default charset=utf8mb4;
+
 create table chats(
   chat_id bigint unsigned not null primary key,
   name varchar(50) default '-',
@@ -11,48 +12,57 @@ create table chats(
   perms tinyint unsigned default 0 not null,
   created int unsigned,
   lastseen int unsigned
-);
+) engine=InnoDB default charset=utf8mb4;
+
 create table modules(
   module varchar(100) not null primary key,
   created int unsigned not null
-);
+) engine=InnoDB default charset=utf8mb4;
+
 insert into modules values
-  ('ProtocolLive\\SimpleTetelegramBot\\StbObjects\\StbAdmin', 0);
+  ('ProtocolLive\\SimpleTelegramBot\\StbObjects\\StbAdmin', 0);
+
 create table commands(
   command varchar(50) not null primary key,
   module varchar(100) not null,
   foreign key(module) references modules(module) on delete cascade on update cascade
-);
+) engine=InnoDB default charset=utf8mb4;
+
 insert into commands values
-  ('admin', 'ProtocolLive\\SimpleTetelegramBot\\StbObjects\\StbAdmin'),
-  ('id','ProtocolLive\\SimpleTetelegramBot\\StbObjects\\StbAdmin');
+  ('admin', 'ProtocolLive\\SimpleTelegramBot\\StbObjects\\StbAdmin'),
+  ('id','ProtocolLive\\SimpleTelegramBot\\StbObjects\\StbAdmin');
+
 create table listeners(
   listener varchar(50) not null,
-  chat_id bigint,
+  chat_id bigint unsigned,
   module varchar(100) not null,
   foreign key(chat_id) references chats(chat_id) on delete cascade,
-  foreign key(module) references modules(module) on delete cascade on update cascade,
+  foreign key(module) references modules(module) on delete cascade,
   unique(listener,chat_id)
-);
+) engine=InnoDB default charset=utf8mb4;
+
 create table sys_logs(
-  log_id int not null primary key auto_increment,
+  log_id int unsigned not null primary key auto_increment,
   time int unsigned not null,
-  chat_id bigint not null,
+  chat_id bigint unsigned not null,
   event varchar(50) not null,
   additional varchar(50),
   foreign key(chat_id) references chats(chat_id) on delete cascade,
   unique(time,chat_id)
-);
+) engine=InnoDB default charset=utf8mb4;
+
 create table sys_params(
   name varchar(50) not null primary key,
   value varchar(50) not null
-);
+) engine=InnoDB default charset=utf8mb4;
+
 insert into sys_params values('DbVersion','1.0.0');
+
 create table variables(
   var_id int unsigned primary key auto_increment,
-  chat_id bigint,
+  chat_id bigint unsigned,
   name varchar(50) not null,
   value varchar(50),
   unique(chat_id,name),
   foreign key(chat_id) references chats(chat_id) on delete cascade
-);
+) engine=InnoDB default charset=utf8mb4;
