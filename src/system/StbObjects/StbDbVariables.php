3610db12fae3d5a3f24af5db07a06508f1003a42
Protocol Corporation <protocollive@outlook.com>
2022-09-11 19:45:27 -0300

    New enum

---
+++ src/system/StbObjects/StbDbVariables.php
@@ -0,0 +1,10 @@
+<?php
+//Protocol Corporation Ltda.
+//https://github.com/ProtocolLive/SimpleTelegramBot
+//2022.09.11.00
+
+namespace ProtocolLive\SimpleTelegramBot\StbObjects;
+
+enum StbDbVariables{
+  case AdminNew;
+}
