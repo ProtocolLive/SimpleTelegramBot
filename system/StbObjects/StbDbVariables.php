1ed651bd839b6faeab4d5ba84811d6c1ca4bd435
Protocol Corporation <protocollive@outlook.com>
2022-12-31 00:39:38 -0300

    New variable

--- system/StbObjects/StbDbVariables.php
+++ system/StbObjects/StbDbVariables.php
@@ -1,10 +1,11 @@
 <?php
 //Protocol Corporation Ltda.
 //https://github.com/ProtocolLive/SimpleTelegramBot
-//2022.09.11.00
+//2022.12.30.00
 
 namespace ProtocolLive\SimpleTelegramBot\StbObjects;
 
 enum StbDbVariables{
+  case Action;
   case AdminNew;
 }
