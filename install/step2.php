e035091c0b7d7983dd16e626de082eec7c8fe131
Protocol Corporation <protocollive@outlook.com>
2023-01-16 18:30:05 -0300

    Using table creation methods

--- install/step2.php
+++ install/step2.php
@@ -1,11 +1,17 @@
 <?php
-//2023.01.16.00
+//2023.01.16.01
 
 use ProtocolLive\PhpLiveDb\{
   Drivers,
-  PhpLiveDb
+  Formats,
+  PhpLiveDb,
+  RefTypes,
+  Types
 };
-use ProtocolLive\SimpleTelegramBot\StbObjects\StbDbAdminPerm;?>
+use ProtocolLive\SimpleTelegramBot\StbObjects\{
+  StbAdmin,
+  StbDbAdminPerm
+};?>
 
 <!DOCTYPE html>
 <html lang="en">
@@ -53,26 +59,265 @@ use ProtocolLive\SimpleTelegramBot\StbObjects\StbDbAdminPerm;?>
       $_POST['pwd'],
       $_POST['db']
     );
-    $sqls = file_get_contents(__DIR__ . '/sql/mysql/install.sql');
   else:
     $PlDb = new PhpLiveDb(
       "$DirSystem/$DirBot/db.db",
       Driver: Drivers::SqLite
     );
-    $sqls = file_get_contents(__DIR__ . '/sql/sqlite/install.sql');
   endif;
-  $consult = $PlDb->GetCustom();
-  $sqls = explode(';', $sqls);
-  array_pop($sqls);
-  foreach($sqls as $sql):
-    $consult->exec($sql);
-  endforeach;
-  $stm = $consult->prepare('
-    insert into chats(chat_id,created,perms)
-    values(:admin,' . time() . ',' . StbDbAdminPerm::All->value . ')
-  ');
-  $stm->bindValue(':admin', $_POST['admin'], PDO::PARAM_INT);
-  $stm->execute();
+  
+  $consult = $PlDb->Create('callbackshash');
+  $consult->Add(
+    'hash',
+    Formats::Varchar,
+    100,
+    NotNull: true,
+    Primary: true
+  );
+  $consult->Add(
+    'method',
+    Formats::Varchar,
+    255,
+    NotNull: true
+  );
+  $consult->Run();
+
+  $consult = $PlDb->Create('chats');
+  $consult->Add(
+    'chat_id',
+    Formats::IntBig,
+    Unsigned: true,
+    NotNull: true,
+    Primary: true
+  );
+  $consult->Add(
+    'name',
+    Formats::Varchar,
+    50,
+    Default: '-',
+    NotNull: true
+  );
+  $consult->Add(
+    'name2',
+    Formats::Varchar,
+    50
+  );
+  $consult->Add(
+    'nick',
+    Formats::Varchar,
+    50
+  );
+  $consult->Add(
+    'lang',
+    Formats::Varchar,
+    5
+  );
+  $consult->Add(
+    'perms',
+    Formats::IntTiny,
+    Unsigned: true,
+    Default: 0,
+    NotNull: true
+  );
+  $consult->Add(
+    'created',
+    Formats::Int,
+    Unsigned: true
+  );
+  $consult->Add(
+    'lastseen',
+    Formats::Int,
+    Unsigned: true
+  );
+  $consult->Run();
+
+  $consult = $PlDb->Create('modules');
+  $consult->Add(
+    'module',
+    Formats::Varchar,
+    100,
+    NotNull: true,
+    Primary: true
+  );
+  $consult->Add(
+    'created',
+    Formats::Int,
+    Unsigned: true,
+    NotNull: true
+  );
+  $consult->Run();
+  $consult = $PlDb->Insert('modules');
+  $consult->FieldAdd('module', StbAdmin::class, Types::Str);
+  $consult->FieldAdd('created', 0, Types::Int);
+  $consult->Run();
+
+  $consult = $PlDb->Create('commands');
+  $consult->Add(
+    'command',
+    Formats::Varchar,
+    50,
+    NotNull: true,
+    Primary: true
+  );
+  $consult->Add(
+    'module',
+    Formats::Varchar,
+    100,
+    NotNull: true,
+    RefTable: 'modules',
+    RefField: 'module',
+    RefDelete: RefTypes::Cascade,
+    RefUpdate: RefTypes::Cascade
+  );
+  $consult->Run();
+  $consult = $PlDb->Insert('commands');
+  $consult->FieldAdd('module', StbAdmin::class, Types::Str);
+  $consult->FieldAdd('command', 'admin', Types::Str);
+  $consult->Run();
+  $consult->FieldAdd('command', 'id', Types::Str);
+  $consult->Run();
+
+  $consult = $PlDb->Create('listeners');
+  $consult->Add(
+    'listener_id',
+    Formats::Int,
+    Unsigned: true,
+    NotNull: true,
+    Primary: true,
+    AutoIncrement: true
+  );
+  $consult->Add(
+    'listener',
+    Formats::Varchar,
+    50,
+    NotNull: true
+  );
+  $consult->Add(
+    'chat_id',
+    Formats::IntBig,
+    Unsigned: true,
+    RefTable: 'chats',
+    RefField: 'chat_id',
+    RefDelete: RefTypes::Cascade,
+    RefUpdate: RefTypes::Cascade
+  );
+  $consult->Add(
+    'module',
+    Formats::Varchar,
+    100,
+    NotNull: true,
+    RefTable: 'modules',
+    RefField: 'module',
+    RefDelete: RefTypes::Cascade,
+    RefUpdate: RefTypes::Cascade
+  );
+  $consult->Unique(['listener', 'chat_id']);
+  $consult->Run();
+
+  $consult = $PlDb->Create('sys_logs');
+  $consult->Add(
+    'log_id',
+    Formats::Int,
+    Unsigned: true,
+    NotNull: true,
+    Primary: true,
+    AutoIncrement: true
+  );
+  $consult->Add(
+    'time',
+    Formats::Int,
+    Unsigned: true,
+    NotNull: true,
+  );
+  $consult->Add(
+    'chat_id',
+    Formats::IntBig,
+    Unsigned: true,
+    NotNull: true,
+    RefTable: 'chats',
+    RefField: 'chat_id',
+    RefDelete: RefTypes::Cascade,
+    RefUpdate: RefTypes::Cascade
+  );
+  $consult->Add(
+    'event',
+    Formats::Varchar,
+    50,
+    NotNull: true,
+  );
+  $consult->Add(
+    'additional',
+    Formats::Varchar,
+    50
+  );
+  $consult->Unique(['time', 'chat_id']);
+  $consult->Run();
+
+  $consult = $PlDb->Create('sys_params');
+  $consult->Add(
+    'name',
+    Formats::Varchar,
+    50,
+    NotNull: true,
+    Primary: true
+  );
+  $consult->Add(
+    'value',
+    Formats::Varchar,
+    50,
+    NotNull: true
+  );
+  $consult->Run();
+  $consult = $PlDb->Insert('sys_params');
+  $consult->FieldAdd('name', 'DbVersion', Types::Str);
+  $consult->FieldAdd('value', '1.0.0', Types::Str);
+  $consult->Run();
+
+  $consult = $PlDb->Create('variables');
+  $consult->Add(
+    'var_id',
+    Formats::Int,
+    Unsigned: true,
+    Primary: true,
+    AutoIncrement: true
+  );
+  $consult->Add(
+    'module',
+    Formats::Varchar,
+    100,
+    RefTable: 'modules',
+    RefField: 'module',
+    RefDelete: RefTypes::Cascade,
+    RefUpdate: RefTypes::Cascade
+  );
+  $consult->Add(
+    'chat_id',
+    Formats::IntBig,
+    Unsigned: true,
+    RefTable: 'chats',
+    RefField: 'chat_id',
+    RefDelete: RefTypes::Cascade,
+    RefUpdate: RefTypes::Cascade
+  );
+  $consult->Add(
+    'name',
+    Formats::Varchar,
+    50,
+    NotNull: true
+  );
+  $consult->Add(
+    'value',
+    Formats::Varchar,
+    50
+  );
+  $consult->Unique(['module', 'chat_id', 'name']);
+  $consult->Run();
+
+  $consult = $PlDb->Insert('chats');
+  $consult->FieldAdd('chat_id', $_POST['admin'], Types::Int);
+  $consult->FieldAdd('created', time(), Types::Int);
+  $consult->FieldAdd('perms', StbDbAdminPerm::All->value, Types::Int);
+  $consult->Run();
 
   rename(__DIR__, $DirSystem . '/install_' . uniqid());
   rename($DirSystem . '/index.php', $DirSystem . '/index_' . uniqid() . '.php');
