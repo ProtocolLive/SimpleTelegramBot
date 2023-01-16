4eb03c4f249a76a4df47ce61172f9f14c08c855c
Protocol Corporation <protocollive@outlook.com>
2023-01-16 09:02:14 -0300

    Added lines to separate querys

--- install/sql/sqlite/install.sql
+++ install/sql/sqlite/install.sql
@@ -2,6 +2,7 @@ create table callbackshash(
   hash text not null primary key,
   method text not null
 );
+
 create table chats(
   chat_id integer not null primary key,
   name text default '-',
@@ -12,21 +13,26 @@ create table chats(
   created integer unsigned,
   lastseen integer unsigned
 );
+
 create table modules(
   module text not null primary key,
   created integer unsigned not null
 );
+
 insert into modules values(
   'ProtocolLive\SimpleTelegramBot\StbObjects\StbAdmin', 0
 );
+
 create table commands(
   command text not null primary key,
   module text not null,
   foreign key(module) references modules(module) on delete cascade
 );
+
 insert into commands values
 ('admin', 'ProtocolLive\SimpleTelegramBot\StbObjects\StbAdmin'),
 ('id', 'ProtocolLive\SimpleTelegramBot\StbObjects\StbAdmin');
+
 create table listeners(
   listener_id integer not null primary key autoincrement,
   listener text not null,
@@ -36,6 +42,7 @@ create table listeners(
   foreign key(module) references modules(module) on delete cascade,
   unique(listener,chat_id)
 );
+
 create table sys_logs(
   log_id integer not null primary key autoincrement,
   time integer unsigned not null,
@@ -45,6 +52,7 @@ create table sys_logs(
   foreign key(chat_id) references chats(chat_id) on delete cascade,
   unique(time,chat_id)
 );
+
 create table sys_params(
   name text not null primary key,
   value text not null
