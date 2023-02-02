dfed84164429f39e70248400e1419f3d21ec8583
Protocol Corporation <protocollive@outlook.com>
2023-02-02 20:13:26 -0300

    Core files moved to another library

--- system/StbObjects/StbModuleInterface.php
+++
@@ -1,9 +0,0 @@
-<?php
-//2023.01.02.00
-
-namespace ProtocolLive\SimpleTelegramBot\StbObjects;
-
-interface StbModuleInterface{
-  public static function Install():void;
-  public static function Uninstall():void;
-}
