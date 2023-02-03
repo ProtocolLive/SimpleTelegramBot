62b39907bbdf4085a658129aa163fdf39a10dae9
Protocol Corporation <protocollive@outlook.com>
2023-02-02 21:12:02 -0300

    Cron and entry moved to core

--- install/DirBot/index.php
+++ install/DirBot/index.php
@@ -1,14 +1,17 @@
 <?php
 //Protocol Corporation Ltda.
 //https://github.com/ProtocolLive/SimpleTelegramBot
-//2022.12.23.00
+//2023.02.02.00
+
+use ProtocolLive\SimpleTelegramBot\StbObjects\StbBotTools;
 
 const DirBot = __DIR__;
 require(dirname(__DIR__, 1) . '/system/system.php');
 
 ArgV();
+$_GET['a'] ??= '';
 if(isset($_SERVER['Cron'])):
-  require(DirSystem . '/cron.php');
-else:
-  require(DirSystem . '/entry.php');
+  StbBotTools::Cron();
+elseif(method_exists(StbBotTools::class, 'Action_' . $_GET['a'])):
+  call_user_func(StbBotTools::class . '::Action_' . $_GET['a']);
 endif;
