2125231cf433d8d29a45e8a5dce6a3c6a9b5f2ee
Protocol Corporation <protocollive@outlook.com>
2023-04-26 12:35:56 -0300

    File changed to call install class

--- index.php
+++ index.php
@@ -1,38 +1,12 @@
 <?php
 //Protocol Corporation Ltda.
 //https://github.com/ProtocolLive/SimpleTelegramBot
-//2023.03.04.00
+//2023.04.26.00
+
+use ProtocolLive\SimpleTelegramBot\Install\Install;
 
 require(__DIR__ . '/system/php.php');
 const DirBot = '';
 require(__DIR__ . '/vendor/autoload.php');
-set_error_handler('error');
-set_exception_handler('error');
-session_start();
 
-$step = filter_input(INPUT_GET, 'step', FILTER_VALIDATE_INT);
-if(basename(__FILE__) !== 'index.php'):
-  echo 'Protocol SimpleTelegramBot already installed!';
-elseif(empty($step)):
-  require(__DIR__ . '/install/step1.php');
-else:
-  require(__DIR__ . '/install/step' . $step . '.php');
-endif;
-
-function error():never{
-  echo '<p>⚠️ Install error!</p>';
-  echo '<pre>';
-  var_dump(func_get_args());
-  die();
-}
-
-function CopyRecursive(string $From, string $To):void{
-  foreach(glob($From . '/*') as $file):
-    if(is_dir($file)):
-      mkdir($To . '/' . basename($file), 0755, true);
-      CopyRecursive($file, $To . '/' . basename($file));
-    else:
-      copy($file, $To . '/' . basename($file));
-    endif;
-  endforeach;
-}
+$Install = new Install();
