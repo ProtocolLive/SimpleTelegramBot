9590eb594bba19174e4f46363d1b53b6feac2622
Protocol Corporation <protocollive@outlook.com>
2024-01-24 09:31:04 -0300

    New constant

--- system/constants.php
+++ system/constants.php
@@ -1,11 +1,12 @@
 <?php
 //Protocol Corporation Ltda.
 //https://github.com/ProtocolLive/SimpleTelegramBot
-//2023.05.23.00
+//2023.10.10.00
 
 define('DirSystem', dirname(__DIR__));
 const DirModules = DirBot . '/modules';
 const DirTextCmds = DirBot . '/TextCmds';
+const DirWebapps = DirSystem . '/WebApps';
 
 abstract class StbDebug{
   const Trace = 16;
