eb5132588dc76c35137746b9cabdaccbb8cfc887
Protocol Corporation <protocollive@outlook.com>
2024-02-11 14:09:25 -0300

    Logs dir

--- system/php.php
+++ system/php.php
@@ -1,7 +1,7 @@
 <?php
 //Protocol Corporation Ltda.
 //https://github.com/ProtocolLive/SimpleTelegramBot
-//2022.03.07.00
+//2024.02.11.00
 
 declare(strict_types = 1);
 ini_set('display_errors', '0');
@@ -9,4 +9,4 @@ ini_set('display_startup_errors', '1');
 ini_set('error_reporting', '-1');
 ini_set('html_errors', '1');
 ini_set('max_execution_time', '10');
-ini_set('error_log', __DIR__ . '/error.log');
+ini_set('error_log', __DIR__ . '/logs/error.log');
