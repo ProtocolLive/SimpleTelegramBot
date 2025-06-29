0b6766f14a6fb2767e5f43974f7133fe18c13d8a
Protocol Corporation <protocollive@outlook.com>
2025-06-29 18:08:27 -0300

    Forgotten class (???)

--- system/constants.php
+++ system/constants.php
@@ -1,16 +1,9 @@
 <?php
 //Protocol Corporation Ltda.
 //https://github.com/ProtocolLive/SimpleTelegramBot
+//2025.06.29.00
 
 define('DirSystem', dirname(__DIR__));
 const DirModules = DirBot . '/modules';
 const DirTextCmds = DirBot . '/TextCmds';
 const DirWebapps = DirSystem . '/WebApps';
-
-/**
- * @version 2024.02.14.00
- */
-abstract class StbDebug{
-  public const Trace = 16;
-  public const Bot = 32;
-}
